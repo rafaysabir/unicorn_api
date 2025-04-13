d554b161b4fefdcf41b025869e016fc6c9e75de1
Umar Fraooq <143319915+rafaysabir@users.noreply.github.com>
2025-04-13 13:15:27 +0500

    changing in db

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 import os
 from neo4j import GraphDatabase
 
-uri = os.getenv("NEO4J_URI", "bolt://localhost:7687")
-username = os.getenv("NEO4J_USERNAME", "neo4j")
-password = os.getenv("NEO4J_PASSWORD", "1234567890")
+uri = os.getenv("NEO4J_URI")
+username = os.getenv("NEO4J_USERNAME")
+password = os.getenv("NEO4J_PASSWORD")
 
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
